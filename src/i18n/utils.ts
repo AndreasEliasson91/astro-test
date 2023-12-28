8c6cda2697846731a30ad185911594dc40d19eda
Andreas Eliasson <andreaseliasson91@gmail.com>
2023-12-28 21:28:47 +0100

    browser default locale

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,41 +1,48 @@
 import { langUI, pages, defaultLang, defaultPage } from './ui';
 
-export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
-  if (lang in langUI) return lang as keyof typeof langUI;
-  return defaultLang;
-}
+export function getLangFromUrl(url: URL, preferredLang: string) {
+    
+    const [, lang] = url.pathname.split('/');
+
+    if (!lang && url.pathname === '/') {
+        if (preferredLang in langUI) return preferredLang as keyof typeof langUI;
+    }
+    
+    if (lang in langUI) return lang as keyof typeof langUI;
+
+    return defaultLang;
+    }
 
 export function getPageFromUrl(url: URL) {
-  const [, , page] = url.pathname.split('/');
-  if (page in pages) return page as keyof typeof pages;
-  return defaultPage;
+    const [, , page] = url.pathname.split('/');
+    if (page in pages) return page as keyof typeof pages;
+    return defaultPage;
 }
 
 export function useTranslations(lang: keyof typeof langUI) {
-  return function t(key: keyof typeof langUI[typeof defaultLang]) {
-    return langUI[lang][key] || langUI[defaultLang][key];
-  }
+    return function t(key: keyof typeof langUI[typeof defaultLang]) {
+        return langUI[lang][key] || langUI[defaultLang][key];
+    }
 }
 
 export async function getLangFromIP(): Promise<keyof typeof langUI> {
-  try {
-    const response = await fetch('https://ipinfo.io/json?token=eea950ff3eba08');
-    const data = await response.json();
+    try {
+        const response = await fetch('https://ipinfo.io/json?token=eea950ff3eba08');
+        const data = await response.json();
 
-    const countryCode = data.country;
-    const lang = countryLangMap[countryCode] || defaultLang;
-    
-    return lang as keyof typeof langUI;
-  } catch (error) {
-    console.error('Error getting language from IP:', error);
-    return defaultLang;
-  }
+        const countryCode = data.country;
+        const lang = countryLangMap[countryCode] || defaultLang;
+        
+        return lang as keyof typeof langUI;
+    } catch (error) {
+        console.error('Error getting language from IP:', error);
+        return defaultLang;
+    }
 }
 
 const countryLangMap: { [key: string]: keyof typeof langUI } = {
-  'US': 'en',
-  'ES': 'es',
-  'FR': 'fr',
-  'SE': 'sv',
+    'US': 'en',
+    'ES': 'es',
+    'FR': 'fr',
+    'SE': 'sv',
 };
