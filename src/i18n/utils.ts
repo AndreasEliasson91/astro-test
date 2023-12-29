e2f29d4ef26d3a002b4b9c1cd2b00614e421515d
Andreas Eliasson <andreaseliasson91@gmail.com>
2023-12-29 12:46:04 +0100

    Added docstrings for utiliyt funcs

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,5 +1,11 @@
 import { langUI, routes, defaultLang, landingPage } from './ui';
 
+/**
+ * Retrieves the language based on the URL and preferred language.
+ * @param {URL} url - The URL object representing the URL.
+ * @param {string} preferredLang - The preferred language.
+ * @returns {keyof typeof langUI} - The language extracted from the URL or the preferred language.
+ */
 export function getLanguage(url: URL, preferredLang: string) {
     
     const [, lang] = url.pathname.split('/');
@@ -16,6 +22,11 @@ export function getLanguage(url: URL, preferredLang: string) {
     return defaultLang;
     }
 
+/**
+ * Retrieves the route from the given URL.
+ * @param {URL} url - The URL object representing the URL.
+ * @returns {keyof typeof routes} - The route extracted from the URL.
+ */
 export function getRouteFromUrl(url: URL) {
     const [, , route] = url.pathname.split('/');
     if (route in routes) {
@@ -24,13 +35,29 @@ export function getRouteFromUrl(url: URL) {
     return landingPage;
 }
 
+/**
+ * Returns a translation function for the specified language key.
+ * @param {keyof typeof langUI[typeof defaultLang]} key - The language key.
+ * @returns {string} - The translated string for the given key.
+ */
 export function useTranslations(lang: keyof typeof langUI) {
     return function t(key: keyof typeof langUI[typeof defaultLang]) {
         return langUI[lang][key] || langUI[defaultLang][key];
     }
 }
 
+/**
+ * Retrieves the language key based on the user's IP address.
+ * @returns {Promise<keyof typeof langUI>} - The language key.
+ */
 export async function getLangFromIP(): Promise<keyof typeof langUI> {
+    const countryLangMap: { [key: string]: keyof typeof langUI } = {
+        'US': 'en',
+        'ES': 'es',
+        'FR': 'fr',
+        'SE': 'sv',
+    };
+
     try {
         const response = await fetch('https://ipinfo.io/json?token=eea950ff3eba08');
         const data = await response.json();
@@ -45,9 +72,3 @@ export async function getLangFromIP(): Promise<keyof typeof langUI> {
     }
 }
 
-const countryLangMap: { [key: string]: keyof typeof langUI } = {
-    'US': 'en',
-    'ES': 'es',
-    'FR': 'fr',
-    'SE': 'sv',
-};
